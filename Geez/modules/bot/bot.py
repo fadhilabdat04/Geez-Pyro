325ad8d511536e014edc8bd7f68458dbd5062100
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-06-22 18:20:23 +0700

    Update bot.py

--- Geez/modules/bot/bot.py
+++ Geez/modules/bot/bot.py
@@ -76,7 +76,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"**Help For {str(modul_name).upper()}**\n\n"
     for x in commands:
         this_command += f"**Command:** {str(x)}\n ∟ **Function:** {str(commands[x])}\n\n"
-    this_command += "© Geez | RAM"
+    this_command += "© Arab | Premium Userbot"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
